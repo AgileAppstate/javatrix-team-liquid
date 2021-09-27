ff37a70f177b2aab0707991dfd1f724755dcdc0a
Joshua Baldwin <baldwinjd2@appstate.edu>
2021-09-27 13:51:54 -0400

    started scaffold of JavaTrix.java

---
+++ Matrix.java
@@ -0,0 +1,28 @@
+package JavaTrix;
+
+public class Matrix implements Cloneable, Serializable
+{
+    private double[][] vals; // Field to store Matrix data
+
+    /**
+     * Constructs a Matrix object from a 2-D array
+     *
+     * @param A The array to be copied from
+     */
+    public Matrix(double[][] A)
+    {
+        //TODO
+    }
+
+    /**
+     * Constructs an m x n Matrix filled with scalar value s
+     *
+     * @param m The number of rows
+     * @param n The number of columns
+     * @param s The scalar value to fill the Matrix with
+     */
+    public Matrix(int m, int n, double s)
+    {
+        //TODO
+    }
+}
